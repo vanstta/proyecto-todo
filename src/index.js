1aff6ca27cedf3d322db1e4f8a4f8bb1356617e5
Vanina Alejandra Scilletta <Vanina@ARRMS-PF36PDBS.valtech.com>
2021-12-21 13:54:59 -0300

    subo página

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import './styles.css';
+import {Todo, TodoList} from './clases'
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList()
+
+todoList.todos.forEach(todo=> crearTodoHtml(todo));
+
